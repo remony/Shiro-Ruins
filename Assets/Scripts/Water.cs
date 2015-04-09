a966874ecf471e8edc6c43c8c39926b0f2eb7356
remony <sudouglas@dundee.ac.uk>
2015-04-09 13:02:48 +0100

    Added script for water: this adds the tag to the collision

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //gameObject.GetComponent<Renderer>().material.color = Color.red;
+        transform.FindChild("mesh_[05]_[00]").gameObject.tag = "Water";
+        transform.FindChild("Collision").gameObject.tag = "Water";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
